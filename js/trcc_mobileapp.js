44f222272e00b8165c699069fb9f82ec206fe0f3
Sarah R. Blette <51761783+sarawithanh11235-bug@users.noreply.github.com>
2020-12-25 11:38:42 -0500

    TRCC Mobile App
    TRCC Mobile App

---
+++ js/trcc_mobileapp.js
@@ -0,0 +1,10 @@
+// toggle hamburger menu when hamburger icon is clicked
+function hamburgerFunction() {
+    document.getElementById("hamburgerDropdown").classList.toggle("show");
+}
+
+// go back to the previous page when "back" is clicked
+function goBack() {
+    window.history.back();
+}
+
